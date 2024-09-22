673cd111b9993ce91dab36e21c7a0d3261ccaa9b
LiaGen <126721915+LiaGen@users.noreply.github.com>
2024-09-22 18:50:46 +0300

    Update test2.sh

--- test/test2.sh
+++ test/test2.sh
@@ -1,70 +1,12 @@
 #!/usr/bin/env bash
 source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
-# Copyright (c) 2021-2024 tteck
-# Author: tteck (tteckster)
+# Copyright (c) 2024 samohosting.ru
+# Author: samohosting.ru
 # License: MIT
-# https://github.com/tteck/Proxmox/raw/main/LICENSE
+# https://github.com/some_link/LICENSE
 
-function header_info {
-clear
-cat <<"EOF"
-   _____         __  __  ____  _    _  ____   _____ _______ _____ _   _  _____   _____  _    _ 
-  / ____|  /\   |  \/  |/ __ \| |  | |/ __ \ / ____|__   __|_   _| \ | |/ ____| |  __ \| |  | |
- | (___   /  \  | \  / | |  | | |__| | |  | | (___    | |    | | |  \| | |  __  | |__) | |  | |
-  \___ \ / /\ \ | |\/| | |  | |  __  | |  | |\___ \   | |    | | | . ` | | |_ | |  _  /| |  | |
-  ____) / ____ \| |  | | |__| | |  | | |__| |____) |  | |   _| |_| |\  | |__| |_| | \ \| |__| |
- |_____/_/    \_|_|  |_|\____/|_|  |_|\____/|_____/   |_|  |_____|_| \_|\_____(_|_|  \_\\____/                                                                                                
 
-EOF
-}
-header_info
 echo -e "Loading..."
-APP="Docker"
-var_disk="4"
-var_cpu="2"
-var_ram="2048"
-var_os="debian"
-var_version="12"
-variables
-color
-catch_errors
-
-function default_settings() {
-  CT_TYPE="1"
-  PW=""
-  CT_ID=$NEXTID
-  HN=$NSAPP
-  DISK_SIZE="$var_disk"
-  CORE_COUNT="$var_cpu"
-  RAM_SIZE="$var_ram"
-  BRG="vmbr0"
-  NET="dhcp"
-  GATE=""
-  APT_CACHER=""
-  APT_CACHER_IP=""
-  DISABLEIP6="no"
-  MTU=""
-  SD=""
-  NS=""
-  MAC=""
-  VLAN=""
-  SSH="no"
-  VERB="no"
-  echo_default
-}
-
-function update_script() {
-header_info
-if [[ ! -d /var ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
-msg_info "Updating ${APP} LXC"
-apt-get update &>/dev/null
-apt-get -y upgrade &>/dev/null
-msg_ok "Updated ${APP} LXC"
-exit
-}
-
-
-
 cat <<"EOF"
    _____         __  __  ____  _    _  ____   _____ _______ _____ _   _  _____   _____  _    _ 
   / ____|  /\   |  \/  |/ __ \| |  | |/ __ \ / ____|__   __|_   _| \ | |/ ____| |  __ \| |  | |
@@ -74,6 +16,7 @@ cat <<"EOF"
  |_____/_/    \_|_|  |_|\____/|_|  |_|\____/|_____/   |_|  |_____|_| \_|\_____(_|_|  \_\\____/                                                                                                
 
 EOF
+
 read -r -p "Может лучше пива? <y/N> " prompt
 if [[ ${prompt,,} =~ ^(y|yes)$ ]]; then
   msg_info "Ищу ближайший магазин... "
@@ -81,13 +24,4 @@ if [[ ${prompt,,} =~ ^(y|yes)$ ]]; then
 if [[ ${prompt,,} =~ ^(n|no)$ ]]; then
   msg_info "Кто не курит и не пьет, тот здоровеньким помрет!"
   msg_info "Установщик покидает чат"
-
-start
-build_container
-description
-
-
-
-
-
-msg_ok "Completed Successfully!\n"
+  msg_ok "Completed Successfully!\n"
