f27b2b341064fc3716fa36ace6021142cb4e64aa
LiaGen <126721915+LiaGen@users.noreply.github.com>
2024-09-22 18:33:41 +0300

    Update test2.sh

--- test/test2.sh
+++ test/test2.sh
@@ -8,12 +8,13 @@ source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build
 function header_info {
 clear
 cat <<"EOF"
-    ____             __            
-   / __ \____  _____/ /_  __  _____
-  / / / / __ \/ ___/ //_/ _ \/ ___/
- / /_/ / /_/ / /__/ ,< /  __/ /    
-/_____/\____/\___/_/|_|\___/_/     
- 
+   _____         __  __  ____  _    _  ____   _____ _______ _____ _   _  _____   _____  _    _ 
+  / ____|  /\   |  \/  |/ __ \| |  | |/ __ \ / ____|__   __|_   _| \ | |/ ____| |  __ \| |  | |
+ | (___   /  \  | \  / | |  | | |__| | |  | | (___    | |    | | |  \| | |  __  | |__) | |  | |
+  \___ \ / /\ \ | |\/| | |  | |  __  | |  | |\___ \   | |    | | | . ` | | |_ | |  _  /| |  | |
+  ____) / ____ \| |  | | |__| | |  | | |__| |____) |  | |   _| |_| |\  | |__| |_| | \ \| |__| |
+ |_____/_/    \_|_|  |_|\____/|_|  |_|\____/|_____/   |_|  |_____|_| \_|\_____(_|_|  \_\\____/ 
+                                                                                               
 EOF
 }
 header_info
@@ -62,7 +63,6 @@ msg_ok "Updated ${APP} LXC"
 exit
 }
 
-start
 read -r -p "Может лучше пива? <y/N> " prompt
 if [[ ${prompt,,} =~ ^(y|yes)$ ]]; then
   msg_info "Installing Portainer $PORTAINER_LATEST_VERSION"
@@ -76,7 +76,7 @@ if [[ ${prompt,,} =~ ^(y|yes)$ ]]; then
     -v portainer_data:/data \
     portainer/portainer-ce:latest
   msg_ok "Installed Portainer $PORTAINER_LATEST_VERSION"
-
+start
 build_container
 description
 
