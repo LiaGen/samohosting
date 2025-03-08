ca66d755a4f46728bd9485a59a6c356802c7128f
LiaGen <126721915+LiaGen@users.noreply.github.com>
2025-03-08 16:01:11 +0300

    Update s1_minipc_proxmox_host_LCD_sofrware_install.sh

--- s1_minipc_display/s1_minipc_proxmox_host_LCD_sofrware_install.sh
+++ s1_minipc_display/s1_minipc_proxmox_host_LCD_sofrware_install.sh
@@ -1,5 +1,5 @@
 start() {
-    if ! (whiptail --backtitle "S1 miniPC LCD DISPLAY TOOL for PROXMOX" --title "S1 miniPC LCD DISPLAY TOOL for PROXMOX" --yesno "Будет установлен софт для работы с дисплеем S1 miniPC на Proxmox ${APP}?(не закрывайте\уходите из консоли до конца установки)   Начать создание контейнера?" 10 58); then
+    if ! (whiptail --backtitle "S1 miniPC LCD DISPLAY TOOL for PROXMOX" --title "S1 miniPC LCD DISPLAY TOOL for PROXMOX" --yesno "Будет установлен софт для работы с дисплеем S1 miniPC на Proxmox ${APP}?(не закрывайте\уходите из консоли до конца установки) Начать устаановку?" 10 58); then
       clear
       exit_script
       exit
