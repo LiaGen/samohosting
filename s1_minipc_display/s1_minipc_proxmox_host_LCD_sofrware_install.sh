43fe286d9a97ee8af3bafc677680736972d30834
LiaGen <126721915+LiaGen@users.noreply.github.com>
2025-03-08 16:02:13 +0300

    Update s1_minipc_proxmox_host_LCD_sofrware_install.sh

--- s1_minipc_display/s1_minipc_proxmox_host_LCD_sofrware_install.sh
+++ s1_minipc_display/s1_minipc_proxmox_host_LCD_sofrware_install.sh
@@ -1,7 +1,7 @@
 start() {
     if ! (whiptail --backtitle "S1 miniPC LCD DISPLAY TOOL for PROXMOX" --title "S1 miniPC LCD DISPLAY TOOL for PROXMOX" --yesno "Будет установлен софт для работы с дисплеем S1 miniPC на Proxmox ${APP}?(не закрывайте\уходите из консоли до конца установки) Начать устаановку?" 10 58); then
       clear
-      exit_script
+      echo -e "Утсановка прервана пользователем"
       exit
     fi
     echo -e "start_funcion_ended"
