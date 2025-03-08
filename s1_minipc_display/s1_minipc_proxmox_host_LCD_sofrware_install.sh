4c023efdc4c883487d45e1e83aac59cb6cae5c10
LiaGen <126721915+LiaGen@users.noreply.github.com>
2025-03-08 15:55:08 +0300

    Update s1_minipc_proxmox_host_LCD_sofrware_install.sh

--- s1_minipc_display/s1_minipc_proxmox_host_LCD_sofrware_install.sh
+++ s1_minipc_display/s1_minipc_proxmox_host_LCD_sofrware_install.sh
@@ -1,5 +1,4 @@
 start() {
-  if command -v pveversion >/dev/null 2>&1; then
     if ! (whiptail --backtitle "S1 miniPC LCD DISPLAY TOOL for PROXMOX" --title "${APP} LXC" --yesno "Будет установлен софт для работы с дисплеем S1 miniPC на Proxmox ${APP}?             (не закрывайте\уходите из консоли до конца установки)   Начать создание контейнера?" 10 58); then
       clear
       exit_script
